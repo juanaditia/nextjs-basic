fa88969257c6f5fa3f08e7cb9b206193ed37251a
Juan <juanaditia94@gmail.com>
2022-02-17 21:32:53 +0700

    create validation

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,40 +2,85 @@ import { useMutation } from '@apollo/client';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FormEvent, useState } from 'react';
+import { toast } from 'react-toastify';
 import { SignupQuery } from '../service/query';
 
-export default function signup() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [password1, setPassword1] = useState('');
-  const [setRegister] = useMutation(SignupQuery);
+interface DataType {
+  username: string;
+  password: string;
+}
 
+export default function signup() {
+  const [setRegister, { error }] = useMutation(SignupQuery);
+  const [password1, setPassword1] = useState('');
+  const [Values, setValues] = useState<DataType>({
+    username: '',
+    password: '',
+  });
+
+  if (error) {
+    return <p>{`Submission error! ${error.message}`}</p>;
+  }
   const router = useRouter();
 
   const register = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('ok');
-    console.log(username, password, ' : ini username password');
+    console.log(Values, ' : ini username password');
 
-    if (password !== password1) {
-      console.log('password tidak sama : ', password, password1);
+    if (Values.username === '') {
+      return toast.error('Please enter Username');
+    } else if (Values.password === '') {
+      return toast.error('Please enter Password');
+    } else if (password1 === '') {
+      return toast.error('Please enter Confrim Password');
+    }
+
+    if (Values.password !== password1) {
+      // console.log('password tidak sama : ', password, password1);
+      return toast.error('your password not same');
     } else {
       const result = await setRegister({
         variables: {
           input: {
-            username,
-            password,
+            username: Values.username,
+            password: Values.password,
           },
         },
       });
       console.log(result, ' : ini result');
+
+      if (result) {
+        toast.success('Now account is register time to login');
+        router.push('/login');
+      }
     }
+
+    // if (password !== password1) {
+    //   console.log('password tidak sama : ', password, password1);
+    // } else {
+    //   const result = await setRegister({
+    //     variables: {
+    //       input: {
+    //         username,
+    //         password,
+    //       },
+    //     },
+    //   });
+    //   console.log(result, ' : ini result');
+    // }
 
     // if (result.data) {
     //   console.log(result.data);
     // } else {
     //   console.log('error');
     // }
+  };
+
+  const changeHandler = (e: { target: { name: any; value: any } }) => {
+    setValues((prevValues) => {
+      return { ...prevValues, [e.target.name]: e.target.value };
+    });
   };
 
   return (
@@ -56,21 +101,24 @@ export default function signup() {
                     className="px-4 py-1 my-2 rounded-lg border-2 text-center
                      text-black border-[#212121] "
                     placeholder="Username"
-                    value={username}
-                    onChange={(i) => setUsername(i.target.value)}
+                    name="username"
+                    value={Values.username}
+                    onChange={changeHandler}
                   />
                   <input
                     type="text"
                     className="px-4 py-1 my-2 rounded-lg border-2 text-center border-[#212121] "
                     placeholder="Password"
-                    value={password}
-                    onChange={(i) => setPassword(i.target.value)}
+                    name="password"
+                    value={Values.password}
+                    onChange={changeHandler}
                   />
 
                   <input
                     type="text"
                     className="px-4 py-1 my-2 rounded-lg border-2 text-center border-[#212121] "
                     placeholder="Confrim Password"
+                    name="password1"
                     value={password1}
                     onChange={(i) => setPassword1(i.target.value)}
                   />
